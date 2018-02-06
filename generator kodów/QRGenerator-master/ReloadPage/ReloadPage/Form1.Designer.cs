e0158141e5a2d366b4b4cc779f1f4df39f185eed
Tomasz Jagla <tjagla32@gmail.com>
2018-02-06 01:31:03 +0100

    QR2

---
+++ generator kodów/QRGenerator-master/ReloadPage/ReloadPage/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ReloadPage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.pictureBoxQRCode = new System.Windows.Forms.PictureBox();
+            this.comboBoxECC = new System.Windows.Forms.ComboBox();
+            this.textBoxQRCode = new System.Windows.Forms.TextBox();
+            this.iconSize = new System.Windows.Forms.NumericUpDown();
+            this.labelIconsize = new System.Windows.Forms.Label();
+            this.iconPath = new System.Windows.Forms.TextBox();
+            this.labelIcon = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 319);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "zdjęcie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 259);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // pictureBoxQRCode
+            // 
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(43, 105);
+            this.pictureBoxQRCode.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBoxQRCode.Name = "pictureBoxQRCode";
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(82, 79);
+            this.pictureBoxQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxQRCode.TabIndex = 4;
+            this.pictureBoxQRCode.TabStop = false;
+            // 
+            // comboBoxECC
+            // 
+            this.comboBoxECC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxECC.FormattingEnabled = true;
+            this.comboBoxECC.Items.AddRange(new object[] {
+            "L",
+            "M",
+            "Q",
+            "H"});
+            this.comboBoxECC.Location = new System.Drawing.Point(30, 54);
+            this.comboBoxECC.Name = "comboBoxECC";
+            this.comboBoxECC.Size = new System.Drawing.Size(125, 21);
+            this.comboBoxECC.TabIndex = 5;
+            // 
+            // textBoxQRCode
+            // 
+            this.textBoxQRCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxQRCode.Location = new System.Drawing.Point(205, 54);
+            this.textBoxQRCode.Name = "textBoxQRCode";
+            this.textBoxQRCode.Size = new System.Drawing.Size(394, 20);
+            this.textBoxQRCode.TabIndex = 6;
+            this.textBoxQRCode.Text = "1234567890";
+            this.textBoxQRCode.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // iconSize
+            // 
+            this.iconSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.iconSize.Location = new System.Drawing.Point(86, 419);
+            this.iconSize.Name = "iconSize";
+            this.iconSize.Size = new System.Drawing.Size(125, 20);
+            this.iconSize.TabIndex = 13;
+            this.iconSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelIconsize
+            // 
+            this.labelIconsize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelIconsize.AutoSize = true;
+            this.labelIconsize.Location = new System.Drawing.Point(26, 423);
+            this.labelIconsize.Name = "labelIconsize";
+            this.labelIconsize.Size = new System.Drawing.Size(52, 13);
+            this.labelIconsize.TabIndex = 12;
+            this.labelIconsize.Text = "Icon size:";
+            // 
+            // iconPath
+            // 
+            this.iconPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.iconPath.Location = new System.Drawing.Point(270, 420);
+            this.iconPath.Name = "iconPath";
+            this.iconPath.Size = new System.Drawing.Size(148, 20);
+            this.iconPath.TabIndex = 11;
+            // 
+            // labelIcon
+            // 
+            this.labelIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelIcon.AutoSize = true;
+            this.labelIcon.Location = new System.Drawing.Point(233, 423);
+            this.labelIcon.Name = "labelIcon";
+            this.labelIcon.Size = new System.Drawing.Size(31, 13);
+            this.labelIcon.TabIndex = 10;
+            this.labelIcon.Text = "Icon:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 97);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox2.Size = new System.Drawing.Size(220, 275);
+            this.textBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 497);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.iconSize);
+            this.Controls.Add(this.labelIconsize);
+            this.Controls.Add(this.iconPath);
+            this.Controls.Add(this.labelIcon);
+            this.Controls.Add(this.textBoxQRCode);
+            this.Controls.Add(this.comboBoxECC);
+            this.Controls.Add(this.pictureBoxQRCode);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.PictureBox pictureBoxQRCode;
+        private System.Windows.Forms.ComboBox comboBoxECC;
+        private System.Windows.Forms.TextBox textBoxQRCode;
+        private System.Windows.Forms.NumericUpDown iconSize;
+        private System.Windows.Forms.Label labelIconsize;
+        private System.Windows.Forms.TextBox iconPath;
+        private System.Windows.Forms.Label labelIcon;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ generator kodów/QRGenerator-master/ReloadPage/ReloadPage/Form1.cs
@@ -0,0 +1,192 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Drawing.Imaging;
+using QRCoder;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+
+namespace ReloadPage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void RenderQrCode(string TEXT)
+        {
+         
+            QRCodeGenerator.ECCLevel eccLevel = 0;
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(TEXT, eccLevel))
+                {
+                    using (QRCode qrCode = new QRCode(qrCodeData))
+                    {
+
+                        var tmp = qrCode.GetGraphic(3, Color.Black, Color.White,
+                            GetIconBitmap(), 0);
+                        pictureBoxQRCode.BackgroundImage = tmp;
+
+                        pictureBoxQRCode.ToString();
+
+                        this.pictureBoxQRCode.Size = new System.Drawing.Size(pictureBoxQRCode.Width, pictureBoxQRCode.Height);
+
+                        //Set the SizeMode to center the image.
+                        this.pictureBoxQRCode.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                        pictureBoxQRCode.SizeMode = PictureBoxSizeMode.CenterImage;
+                    }
+                }
+            }
+        }
+
+        public void saveToFile(string read, string write)
+        {
+
+            //var lines = File.ReadAllLines(read);
+
+            //lines = lines.Skip(1).ToArray();
+            //File.AppendAllLines(write, lines);
+
+          
+            //textBox2.Text += line.Substring(0, line.IndexOf(";"));
+ 
+
+
+            var lines = File.ReadLines(read);
+            int row = 0;
+            int row2 = 0;
+            foreach (var line in lines)
+            {
+                if (row != 0)
+                {
+                    textBox2.Text += line.Substring(0, line.IndexOf(";"));
+                    //SKU[row2] = textBox2.Text;
+                    row2++;
+                    textBox2.Text += Environment.NewLine;
+                }
+                row++;
+                
+            }
+
+        }
+
+        private Bitmap GetIconBitmap()
+        {
+            Bitmap img = null;
+            if (iconPath.Text.Length > 0)
+            {
+                try
+                {
+                    img = new Bitmap(iconPath.Text);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return img;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            RenderQrCode(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            var lines = File.ReadLines("C:/Users/Tomek/Desktop/CSVK-Baterie.csv");
+            int row = 0;
+
+            foreach (var line in lines)
+            {
+
+                if (row != 0)
+                {
+                    var doc = new iTextSharp.text.Document(iTextSharp.text.PageSize.A4, 0f, 0f, 0f, 0f);
+                    //textBox2.Text = line.Substring(0, line.IndexOf(";"));
+                    RenderQrCode(line.Substring(0, line.IndexOf(";")));
+
+                    PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(@"C:\Users\Tomek\Desktop\BaterieQR\" + line.Substring(0, line.IndexOf(";")) + ".pdf", FileMode.Create));
+                    doc.Open();
+                    doc.SetMargins(0f, 0f, 0f, 0f);
+
+                    Bitmap qr_image = new Bitmap(pictureBoxQRCode.BackgroundImage);
+                    qr_image = addTextToImage(qr_image, line.Substring(0, line.IndexOf(";")));
+
+
+                    iTextSharp.text.Image pdfImage = iTextSharp.text.Image.GetInstance(qr_image, System.Drawing.Imaging.ImageFormat.Bmp);
+
+
+                    PdfPTable table = new PdfPTable(6);
+
+                    float tmp = 24.75F;
+                    table.HorizontalAlignment = Element.ALIGN_LEFT;
+                    table.WidthPercentage = 100;
+                    table.DefaultCell.FixedHeight = iTextSharp.text.Utilities.MillimetersToPoints(tmp);
+                    table.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.DefaultCell.PaddingTop = 1;
+                    table.DefaultCell.PaddingBottom = 1;
+
+                    for (int j = 0; j < 72; j++)
+                    {
+                        pdfImage.ScaleAbsolute(30, 25);
+                        table.AddCell(pdfImage);
+                    }
+
+
+                    doc.Add(table);
+                    doc.Close();
+                }
+                row++;
+
+
+            }
+
+            
+
+            textBox1.Text = "chyba działa";
+        }
+        private Bitmap addTextToImage(Bitmap image, string text)
+        {
+            Bitmap tmp;
+            using (Bitmap newbitmap = new Bitmap(image))
+            {
+                PointF firstLocation = new PointF(0, 0);
+                using (Graphics graphics = Graphics.FromImage(newbitmap))
+                {
+                    using (System.Drawing.Font arialFont = new System.Drawing.Font("Arial", 5))
+                    {
+                        //graphics.DrawImage(newbitmap, 0, 0, iTextSharp.text.Utilities.MillimetersToPoints(80), iTextSharp.text.Utilities.MillimetersToPoints(50));
+                        graphics.DrawString(text, arialFont, Brushes.Black, firstLocation);
+                        
+                        //graphics.DrawString(secondText, arialFont, Brushes.Red, secondLocation);
+                    }
+                }
+                tmp = new Bitmap(newbitmap);
+                tmp.SetResolution(300, 240);
+            }
+            return tmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
